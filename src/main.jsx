e74b218a0d5ba6ddcd6cd3edb3b84d41222a95b0
Murua-Alvaro <alvaro.murua.j@gmail.com>
2026-09-13 17:01:28 -0700

    Activa inventario territorial como demo principal de SLTM

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import './styles.css';
-import AppV4 from './AppV4';
+import TerrainInventory from './TerrainInventory';
 
-createRoot(document.getElementById('root')).render(<AppV4 />);
+createRoot(document.getElementById('root')).render(<TerrainInventory />);
