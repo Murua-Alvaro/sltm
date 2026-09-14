06af076f017b98d803bb6444db3a1250616d1990
Murua-Alvaro <alvaro.murua.j@gmail.com>
2026-09-13 23:18:38 -0700

    Activa nueva interfaz profesional de operación territorial

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
-import FieldLiveApp from './FieldLiveApp';
+import ModernFieldApp from './ModernFieldApp';
 
-createRoot(document.getElementById('root')).render(<FieldLiveApp />);
+createRoot(document.getElementById('root')).render(<ModernFieldApp />);
 
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
