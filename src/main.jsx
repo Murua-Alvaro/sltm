78798c4b4b528940c65d0d81262d157866a21be9
Murua-Alvaro <alvaro.murua.j@gmail.com>
2026-09-13 20:40:47 -0700

    Carga pulido visual del modo de campo SLTM

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import './terrain-extra.css';
 import TerrainInventory from './TerrainInventory';
 
 createRoot(document.getElementById('root')).render(<TerrainInventory />);
